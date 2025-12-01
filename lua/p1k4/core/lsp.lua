31cec9e6a3d7962583db4bab817ccd19b2900e98
Piyush Keshan <piyushkeshan@Heron.local>
2025-12-01 06:40:26 +0530

    .

--- lua/p1k4/core/lsp.lua
+++ lua/p1k4/core/lsp.lua
@@ -3,8 +3,28 @@ vim.lsp.config.clangd = {
 	root_markers = { "compile_commands.json", "compile_flags.txt" },
 	filetypes = { "c", "cpp" },
 }
+vim.lsp.config.pylsp = {
+	cmd = { "pylsp" }, -- or full path: "/path/to/.venv/bin/pylsp"
+	root_markers = { "pyproject.toml", "setup.cfg", "setup.py", "requirements.txt", ".git" },
+	filetypes = { "python" },
+	settings = {
+		-- pylsp plugin settings (enable/disable plugins as you prefer)
+		pylsp = {
+			plugins = {
+				pyflakes = { enabled = true },
+				pycodestyle = { enabled = false }, -- disable if you use ruff/black
+				pyls_mypy = { enabled = false },
+				yapf = { enabled = false },
+				black = { enabled = true }, -- enable black if installed
+				pylsp_rope = { enabled = false }, -- example: disable rope if not used
+			},
+		},
+	},
+}
 
 vim.lsp.enable({ "clangd" })
+vim.lsp.enable({ "pylsp" })
+
 vim.diagnostic.config({
 	-- Use the default configuration
 	virtual_lines = true,
--- lua/p1k4/plugins/treesitter.lua
+++ lua/p1k4/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	dependencies = { "OXY2DEV/markview.nvim" },
+	lazy = false,
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
